9adc317a999d883297df2d933237f0f13f7955bf
Paul Rupke <paul.rupke@gmail.com>
2020-09-05 15:29:19 -0400

    Updated table styling, icon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Popover, PopoverHeader, PopoverBody} from 'reactstrap';
+import Table from './components/Table/Table';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const toggle = () => setPopoverOpen(!popoverOpen);
+
+    return (
+      <div className="App">
+        <Tabs className="navbar-tabs">
+          <TabList>
+              <Tab>1</Tab>
+              <Tab>2</Tab>
+              <Tab>3</Tab>
+          </TabList>
+          <TabPanel>
+            <h2>tab 1 content</h2>
+            <Table />
+          </TabPanel>
+          <TabPanel>
+            <h2>tab 2 content</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>tab 3 content</h2>
+          </TabPanel>
+        </Tabs>
+
+      </div>
+    );
+  
 }
-
-export default App;
