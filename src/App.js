b2c340e4fcc8acb177b4bcd42c76dbf077bf9508
Paul Rupke <paul.rupke@gmail.com>
2020-10-25 12:25:33 -0400

    Separate Login component, add react router

--- src/App.js
+++ src/App.js
@@ -1,63 +1,32 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './components/Login/Login';
 import './App.css';
-import { Button, Popover, PopoverHeader, PopoverBody} from 'reactstrap';
-import Table from './components/Table/Table';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { getCookie, setCookie } from './assets/Cookies';
 
 export default function App() {
+  let user = getCookie("user") === null ? setCookie("user") : getCookie("user");
 
-  const [popoverOpen, setPopoverOpen] = useState(false);
+  // const [popoverOpen, setPopoverOpen] = useState(false);
   // const toggle = () => setPopoverOpen(!popoverOpen);
-  let noToken, loggedIn = false;
+  const [loginStatus, setLoggedIn] = useState(false);
 
-    return (
-      <div className="App">
-        {loggedIn === false && (
-            <div class="login-container">
-              <h1>Slow<span>Draft</span></h1>
-              <p>Fantasy hockey drafting at your own pace</p>
-              <p>Currently by invitation only</p>
-                { noToken && (
-                  <div>
-                  Unable to get access token.
-                  <p>Response:</p>
-                  <p>Error message:</p>
-                </div>
-                )}
-                <div class="connect-to-yahoo">
-                  <div>
-                    <Button class="connect-button" href="{{yahoo_auth_url}}">Sign in with Yahoo
-                      {/* <img alt="Yahoo" src="" width="82.3" height="14"> */}
-                    </Button>
-                  </div>
-                </div>
-        
-            </div>	
-        )}
-
-        {loggedIn === true && (
-
-          <Tabs className="navbar-tabs">
-            <TabList>
-                <Tab>1</Tab>
-                <Tab>2</Tab>
-                <Tab>3</Tab>
-            </TabList>
-            <TabPanel>
-              <h2>tab 1 content</h2>
-              <Table />
-            </TabPanel>
-            <TabPanel>
-              <h2>tab 2 content</h2>
-            </TabPanel>
-            <TabPanel>
-              <h2>tab 3 content</h2>
-            </TabPanel>
-          </Tabs>
-
-        )}
-      </div>
-    );
-  
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Login 
+            loginStatus={loginStatus}
+            login={setLoggedIn}
+          />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
