d7217560c657a4ed01b90c03ff25b4eebbdd5475
Paul Rupke <paul.rupke@gmail.com>
2020-10-25 12:26:01 -0400

    Add get and set cookies functions

---
+++ src/assets/Cookies.jsx
@@ -0,0 +1,22 @@
+
+export const setCookie = (string, value) => {
+  const expiry = new Date(); 
+  expiry.setDate(expiry.getDate() + 90)
+  document.cookie = string + "=" + value + "; expires=" + expiry
+}
+
+export const getCookie = (cookieName) => {
+  let name = cookieName + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
