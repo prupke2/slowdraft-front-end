5e0d576428c5895ed19b3d1dc1a40342ea9b333b
Paul Rupke <paul.rupke@gmail.com>
2020-10-29 22:20:53 -0400

    Update navbar, add it to main app

--- src/App.js
+++ src/App.js
@@ -6,27 +6,34 @@ import {
   Link
 } from "react-router-dom";
 import Login from './components/Login/Login';
+import Navbar from './components/Navbar/Navbar';
+import Table from './components/Table/Table';
 import './App.css';
 import 'react-tabs/style/react-tabs.css';
 import { getCookie, setCookie } from './assets/Cookies';
 
 export default function App() {
-  let user = getCookie("user") === null ? setCookie("user") : getCookie("user");
-
+  // const user = getCookie("user") === null ? setCookie("user") : getCookie("user");
   // const [popoverOpen, setPopoverOpen] = useState(false);
   // const toggle = () => setPopoverOpen(!popoverOpen);
   const [loginStatus, setLoggedIn] = useState(false);
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Login 
-            loginStatus={loginStatus}
-            login={setLoggedIn}
-          />
-        </Route>
-      </Switch>
-    </Router>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login 
+              loginStatus={loginStatus}
+              login={setLoggedIn}
+            />
+          </Route>
+          <Route exact path="/navbar">
+            <Navbar />
+            <Table />
+          </Route>
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,29 +2,24 @@ import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 // import 'react-tabs/style/react-tabs.css';
 
-
-export default class Navbar extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Tabs className="navbar-tabs">
-          <TabList>
-            <Tab>1</Tab>
-            <Tab>2</Tab>
-            <Tab>3</Tab>
-          </TabList>
-          <TabPanel>
-            <h2>tab 1 content</h2>
-          </TabPanel>
-          <TabPanel>
-            <h2>tab 2 content</h2>
-          </TabPanel>
-          <TabPanel>
-            <h2>tab 3 content</h2>
-          </TabPanel>
-        </Tabs>
-
-      </React.Fragment>
-    );
-  }
+export default function Navbar() {
+  return (
+    <Tabs className="navbar-tabs">
+      <TabList>
+        <Tab>1</Tab>
+        <Tab>2</Tab>
+        <Tab>3</Tab>
+      </TabList>
+      <TabPanel>
+        <h2>tab 1 content</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>tab 2 content</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>tab 3 content</h2>
+      </TabPanel>
+    </Tabs>
+  );
 }
+
