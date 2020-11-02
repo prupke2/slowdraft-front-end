ac86bc03bbc2068329cf28585ba43e0f1fbe09c7
Paul Rupke <paul.rupke@gmail.com>
2020-11-01 21:05:39 -0500

    Add test button to navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 // import 'react-tabs/style/react-tabs.css';
 
 export default function Navbar() {
+  const [currentTime, setCurrentTime] = useState();
+  function update() {
+    fetch('/test').then(res => res.json()).then(data => {
+      setCurrentTime(data.test);
+    });
+  }
+
+  // useEffect example
+  // useEffect(() => {
+  //   fetch('/test').then(res => res.json()).then(data => {
+  //     setCurrentTime(data.test);
+  //   });
+  // }, [update]);
+
   return (
     <Tabs className="navbar-tabs">
       <TabList>
@@ -12,6 +26,8 @@ export default function Navbar() {
       </TabList>
       <TabPanel>
         <h2>tab 1 content</h2>
+        <button onClick={update}>test</button>
+        Test: {currentTime}
       </TabPanel>
       <TabPanel>
         <h2>tab 2 content</h2>
