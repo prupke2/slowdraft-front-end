eddb29f7488edceee01c9fa30f400a4a78b12c3d
Paul Rupke <paul.rupke@gmail.com>
2020-11-06 15:32:27 -0500

    Clean App.js- use tabs instead of router, add login check

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
-import Table from './components/Table/Table';
+import Login from './components/Login/Login';
 import './App.css';
-import 'react-tabs/style/react-tabs.css';
 // import { getCookie, setCookie } from './assets/Cookies';
 
 export default function App() {
   // const user = getCookie("user") === null ? setCookie("user") : getCookie("user");
-  // const [popoverOpen, setPopoverOpen] = useState(false);
-  // const toggle = () => setPopoverOpen(!popoverOpen);
-  const [loginStatus, setLoggedIn] = useState(false);
+  const [loginStatus, setLoginStatus] = useState(true);
 
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login 
-              loginStatus={loginStatus}
-              login={setLoggedIn}
-            />
-          </Route>
-          <Route exact path="/navbar">
-            <Navbar />
-            <Table />
-          </Route>
-        </Switch>
-      </Router>
+      { !loginStatus && (
+        <Login 
+          loginStatus={loginStatus}
+          login={setLoginStatus}
+        />
+      )}
+      { loginStatus && (
+        <Navbar />
+      )}
     </React.Fragment>
   );
 }
