8da10880c383fc22d1b1b5e4f7fe7af539c08a32
Paul Rupke <paul.rupke@gmail.com>
2020-11-07 09:30:12 -0500

    Update navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './Navbar.css';
 // import 'react-tabs/style/react-tabs.css';
@@ -19,7 +19,7 @@ export default function Navbar() {
   // }, [update]);
 
   return (
-    <Tabs className="navbar-tabs">
+    <Tabs defaultIndex="0" className="navbar-tabs">
       <TabList>
         <Tab>1</Tab>
         <Tab>2</Tab>
