28a43ee8d0b188367d6940369fafc6c4e42c608e
Paul Rupke <paul.rupke@gmail.com>
2020-11-08 11:02:07 -0500

    Add call to get oauth datta

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Login from './components/Login/Login';
+import qs from 'query-string';
 import './App.css';
-// import { getCookie, setCookie } from './assets/Cookies';
+import { getCookie, setCookie } from './assets/Cookies';
 
 export default function App() {
-  // const user = getCookie("user") === null ? setCookie("user") : getCookie("user");
-  const [loginStatus, setLoginStatus] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const accessCode = getCookie("accessCode");
+  const [oauthData, setOauthData] = useState(null);
+
+  const queryParams = qs.parse(window.location.search);
+  const code = queryParams["code"];
+
+  function getOauthData() {
+    fetch('/oauth_data', {
+      method: 'GET',
+    })
+    .then(response => {
+      console.log("response: " + JSON.stringify(response, null, 4))
+      return response.json()
+    })
+    .then(response => {
+      setOauthData(response)
+      console.log("response 2: " + JSON.stringify(response, null, 4))
+      return
+    })
+    .then(log => {
+      console.log("oauthData: " + oauthData);
+    })
+  }
+
+  if (code) {
+    fetch('/login', {
+      method: 'GET',
+    })
+  }
+
+  if (!loggedIn && oauthData === null) {
+    getOauthData()
+  }
 
   return (
     <React.Fragment>
-      { !loginStatus && (
+      { !loggedIn && (
         <Login 
-          loginStatus={loginStatus}
-          login={setLoginStatus}
+          loggedIn={loggedIn}
+          login={setLoggedIn}
+          code={code}
+          onClick={getOauthData}
+          oauthData={oauthData}
         />
       )}
-      { loginStatus && (
+
+      { loggedIn && (
         <Navbar />
       )}
     </React.Fragment>
