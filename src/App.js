8fff81218b1f5e1143c5e3a39ac3b9db1c75c91b
Paul Rupke <paul.rupke@gmail.com>
2020-12-06 21:46:08 -0500

    Add hoc, remove old oauth function

--- src/App.js
+++ src/App.js
@@ -3,59 +3,47 @@ import Navbar from './components/Navbar/Navbar';
 import Login from './components/Login/Login';
 import qs from 'query-string';
 import './App.css';
-import { getCookie, setCookie } from './assets/Cookies';
+import Aux from './hoc/Aux';
+import { getCookie } from './assets/Cookies';
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const accessCode = getCookie("accessCode");
-  const [oauthData, setOauthData] = useState(null);
-
+  // const [oauthData, setOauthData] = useState(null);
   const queryParams = qs.parse(window.location.search);
-  const code = queryParams["code"];
 
-  function getOauthData() {
-    fetch('/oauth_data', {
-      method: 'GET',
-    })
-    .then(response => {
-      console.log("response: " + JSON.stringify(response, null, 4))
-      return response.json()
-    })
-    .then(response => {
-      setOauthData(response)
-      console.log("response 2: " + JSON.stringify(response, null, 4))
-      return
-    })
-    .then(log => {
-      console.log("oauthData: " + oauthData);
-    })
-  }
+  // function getOauthData() {
+  //   fetch('/oauth_data', {
+  //     method: 'GET',
+  //   })
+  //   .then(response => {
+  //     console.log("response: " + JSON.stringify(response, null, 4))
+  //     return response.json()
+  //   })
+  //   .then(response => {
+  //     setOauthData(response)
+  //     console.log("response 2: " + JSON.stringify(response, null, 4))
+  //     return
+  //   })
+  //   .then(log => {
+  //     console.log("oauthData: " + oauthData);
+  //   })
+  // }
 
-  if (code) {
-    fetch('/login', {
-      method: 'GET',
-    })
-  }
-
-  if (!loggedIn && oauthData === null) {
-    getOauthData()
-  }
+  const code = getCookie('access_token') || queryParams["code"];
 
   return (
-    <React.Fragment>
+    <Aux>
       { !loggedIn && (
         <Login 
           loggedIn={loggedIn}
           login={setLoggedIn}
           code={code}
-          onClick={getOauthData}
-          oauthData={oauthData}
         />
       )}
 
       { loggedIn && (
         <Navbar />
       )}
-    </React.Fragment>
+    </Aux>
   );
 }
