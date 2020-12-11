236558e0673844cb120a9102f42d1f7d7a6a9923
Paul Rupke <paul.rupke@gmail.com>
2020-12-11 14:34:01 -0500

    Move useInput into chat component

---
+++ src/components/AppWrapper/Chat/useInput.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export default function useInput()
+{
+  let [value, setValue] = useState('');
+  let onChange = function(event){
+    setValue(event.target.value);
+  };
+  return {
+    value,
+    setValue,
+    onChange
+  };
+}
