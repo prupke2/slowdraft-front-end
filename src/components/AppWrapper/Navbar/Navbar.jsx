d4a9d8daf80355a84e9e85ae02ad785071dc735c
Paul Rupke <paul.rupke@gmail.com>
2020-12-13 22:51:59 -0500

    add basic modal

--- src/components/AppWrapper/Navbar/Navbar.jsx
+++ src/components/AppWrapper/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './Navbar.css';
 import PlayersTab from '../Tabs/PlayersTab/PlayersTab';
 import TeamTab from '../Tabs/TeamTab/TeamTab';
 import ForumTab from '../Tabs/ForumTab/ForumTab';
+import Modal from 'react-modal';
 
 export default function Navbar({logout}) {
+
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
 
   return (
     <>
@@ -16,6 +33,16 @@ export default function Navbar({logout}) {
           <Tab>Forum</Tab>
         </TabList>
         <TabPanel>
+          <button onClick={openModal}>test</button>
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+          >
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+          </Modal>
           <PlayersTab />
         </TabPanel>
         <TabPanel>
