46c12b5b7042d2ed3808d33215514b541024f8f5
Paul Rupke <paul.rupke@gmail.com>
2020-12-14 21:45:37 -0500

    Seperate Pagination into its own component

---
+++ src/components/Table/Pagination/Pagination.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './Pagination.css';
+
+export default function Pagination(
+    { 
+      gotoPage, canPreviousPage, previousPage, nextPage,
+      canNextPage, pageCount, pageIndex, pageOptions
+    }) {
+  return (
+
+    <ul className="pagination">
+      <li className="pagination-arrows">
+        <div className="page-item" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            <span className="page-link">First</span>
+        </div>
+        <div className="page-item" onClick={() => previousPage()} disabled={!canPreviousPage}>
+            <span className="page-link">{'<'}</span>
+        </div>
+        <div className="page-item" onClick={() => nextPage()} disabled={!canNextPage}>
+            <span className="page-link">{'>'}</span>
+        </div>
+        <div className="page-item" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+            <span className="page-link">Last</span>
+        </div>
+      </li>
+
+      <li className="pagination-goto-page">
+        <span className="page-link">
+          Page&nbsp;
+          <div className="page-of-page">
+            <strong>
+                {pageIndex + 1} of {pageOptions.length}
+            </strong>{' '}
+          </div>
+        </span>
+        <span className="page-link">
+          <div>Go&nbsp;to:</div>
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                gotoPage(page)
+            }}
+            style={{ width: '40px', height: '22px' }}
+          />
+        </span>
+      </li>{' '}
+      {/* <select
+          className="form-control"
+          value={pageSize}
+          onChange={e => {
+              setPageSize(Number(e.target.value))
+          }}
+          style={{ maxWidth: '150px', height: '33px' }}
+      >
+          {[5, 10, 20, 30, 40, 50].map(pageSize => (
+              <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+              </option>
+          ))}
+      </select> */}
+    </ul>
+  );
+}
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,6 +3,7 @@ import ReactHtmlParser from 'react-html-parser';
 import { useTable, useFilters, useSortBy, usePagination } from 'react-table';
 import { matchSorter } from 'match-sorter';
 import './Table.css';
+import Pagination from "./Pagination/Pagination";
 
 export default function Table({ columns, data, defaultColumnFilter, tableState }) {
 
@@ -108,6 +109,16 @@ export default function Table({ columns, data, defaultColumnFilter, tableState }
           {JSON.stringify({ pageIndex, pageSize, pageCount, canNextPage, canPreviousPage}, null, 2)}
         </code>
       </pre> */}
+      <Pagination 
+        gotoPage={gotoPage}
+        previousPage={previousPage}
+        canPreviousPage={canPreviousPage}
+        nextPage={nextPage}
+        canNextPage={canNextPage}
+        pageCount={pageCount}
+        pageIndex={pageIndex}
+        pageOptions={pageOptions}
+      />
       <table className="table" {...getTableProps()}>
         <thead>
           {headerGroups.map(headerGroup => (
@@ -190,60 +201,16 @@ export default function Table({ columns, data, defaultColumnFilter, tableState }
           })}
         </tbody>
       </table>
-
-      <ul className="pagination">
-        <li className="pagination-arrows">
-          <div className="page-item" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-              <span className="page-link">First</span>
-          </div>
-          <div className="page-item" onClick={() => previousPage()} disabled={!canPreviousPage}>
-              <span className="page-link">{'<'}</span>
-          </div>
-          <div className="page-item" onClick={() => nextPage()} disabled={!canNextPage}>
-              <span className="page-link">{'>'}</span>
-          </div>
-          <div className="page-item" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-              <span className="page-link">Last</span>
-          </div>
-        </li>
-
-        <li className="pagination-goto-page">
-          <span className="page-link">
-            Page&nbsp;
-            <div className="page-of-page">
-              <strong>
-                  {pageIndex + 1} of {pageOptions.length}
-              </strong>{' '}
-            </div>
-          </span>
-          <span className="page-link">
-            <div>Go&nbsp;to:</div>
-            <input
-              type="number"
-              defaultValue={pageIndex + 1}
-              onChange={e => {
-                  const page = e.target.value ? Number(e.target.value) - 1 : 0
-                  gotoPage(page)
-              }}
-              style={{ width: '40px', height: '22px' }}
-            />
-          </span>
-        </li>{' '}
-        {/* <select
-            className="form-control"
-            value={pageSize}
-            onChange={e => {
-                setPageSize(Number(e.target.value))
-            }}
-            style={{ maxWidth: '150px', height: '33px' }}
-        >
-            {[5, 10, 20, 30, 40, 50].map(pageSize => (
-                <option key={pageSize} value={pageSize}>
-                    Show {pageSize}
-                </option>
-            ))}
-        </select> */}
-      </ul>
+      <Pagination 
+        gotoPage={gotoPage}
+        previousPage={previousPage}
+        canPreviousPage={canPreviousPage}
+        nextPage={nextPage}
+        canNextPage={canNextPage}
+        pageCount={pageCount}
+        pageIndex={pageIndex}
+        pageOptions={pageOptions}
+      />
     </div >
   )
 }
