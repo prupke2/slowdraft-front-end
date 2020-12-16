3cc66243227e27f9b2cdd9a5f8464cc276f90399
Paul Rupke <paul.rupke@gmail.com>
2020-12-15 22:59:49 -0500

    Add FilterTypes

--- src/components/Table/FilterTypes/FilterTypes.jsx
+++ src/components/Table/FilterTypes/FilterTypes.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './FilterTypes.css';
 
 export function SearchColumnFilter({
     column: { filterValue, setFilter, id },
@@ -15,3 +16,52 @@ export function SearchColumnFilter({
       />
     )
   }
+
+export function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder="Filter..."
+    />
+  )
+}  
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+export function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach(row => {
+      options.add(row.values[id])
+    })
+    return [...options.values()]
+  }, [id, preFilteredRows])
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={e => {
+        setFilter(e.target.value || undefined)
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  )
+}
