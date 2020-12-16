24236ff84c4790e2f012832f8fd0c5be5b668b88
Paul Rupke <paul.rupke@gmail.com>
2020-12-15 23:00:37 -0500

    Move Modal test out of navbar component

--- src/components/AppWrapper/Navbar/Navbar.jsx
+++ src/components/AppWrapper/Navbar/Navbar.jsx
@@ -4,45 +4,18 @@ import './Navbar.css';
 import PlayersTab from '../Tabs/PlayersTab/PlayersTab';
 import TeamTab from '../Tabs/TeamTab/TeamTab';
 import ForumTab from '../Tabs/ForumTab/ForumTab';
-import Modal from 'react-modal';
 
 export default function Navbar({logout}) {
 
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    // subtitle.style.color = '#f00';
-  }
-
-  function closeModal(){
-    setIsOpen(false);
-  }
-
   return (
     <>
-      <Tabs defaultIndex={1} className="navbar-tabs">
+      <Tabs defaultIndex={0} className="navbar-tabs">
         <TabList>
           <Tab>Players</Tab>
           <Tab>Team</Tab>
           <Tab>Forum</Tab>
         </TabList>
         <TabPanel>
-          <button onClick={openModal}>test</button>
-          <Modal
-            isOpen={modalIsOpen}
-            onAfterOpen={afterOpenModal}
-            onRequestClose={closeModal}
-            contentLabel="Example Modal"
-          >
-            <button onClick={closeModal}>close</button>
-            <div>I am a modal</div>
-          </Modal>
           <PlayersTab />
         </TabPanel>
         <TabPanel>
