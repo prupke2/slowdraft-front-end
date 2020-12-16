94fa53e9ab6fdda2c547c09296b7f624fac2c34e
Paul Rupke <paul.rupke@gmail.com>
2020-12-15 22:59:59 -0500

    Add Yahoo StatIndex

---
+++ src/components/AppWrapper/Tabs/PlayersTab/StatIndex.jsx
@@ -0,0 +1,16 @@
+// Yahoo stat_id mappings - can comment out ones that are not needed
+SKATER_STAT_INDEX = {
+	'careerGP' : 'career GP',
+	'0' : 'GP',
+	'1' : 'G',
+	'2' : 'A',
+	'3' : 'P',
+	'4' : '+/-',
+	'5' : 'PIM',
+	'8' : 'PPP',
+	'14' : 'SOG',
+	'15' : 'S%',
+	'16' : 'FW',
+	'31' : 'HIT',
+	'32' : 'BLK'
+}
