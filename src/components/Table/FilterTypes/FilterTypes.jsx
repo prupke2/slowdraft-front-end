b14cc52d779d12db8e6bde346afe526e10ea2ab0
Paul Rupke <paul.rupke@gmail.com>
2020-12-18 23:00:13 -0500

    Add custom filter types

--- src/components/Table/FilterTypes/FilterTypes.jsx
+++ src/components/Table/FilterTypes/FilterTypes.jsx
@@ -20,7 +20,7 @@ export function SearchColumnFilter({
 export function DefaultColumnFilter({
   column: { filterValue, preFilteredRows, setFilter },
 }) {
-  const count = preFilteredRows.length
+  // const count = preFilteredRows.length
 
   return (
     <input
@@ -33,9 +33,13 @@ export function DefaultColumnFilter({
   )
 }  
 
+
+
+
+
 // This is a custom filter UI for selecting
 // a unique option from a list
-export function SelectColumnFilter({
+export function SelectPlayerTypeColumnFilter({
   column: { filterValue, setFilter, preFilteredRows, id },
 }) {
   // Calculate the options for filtering
@@ -48,6 +52,11 @@ export function SelectColumnFilter({
     return [...options.values()]
   }, [id, preFilteredRows])
 
+  const optionToProspect = {
+    "0": "Non-prospects",
+    "1": "Prospects"
+  }
+
   // Render a multi-select box
   return (
     <select
@@ -59,9 +68,45 @@ export function SelectColumnFilter({
       <option value="">All</option>
       {options.map((option, i) => (
         <option key={i} value={option}>
-          {option}
+          {optionToProspect[option]}
         </option>
       ))}
+    </select>
+  )
+}
+
+
+export function SelectPositionColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  const options = React.useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach(row => {
+      options.add(row.values[id])
+    })
+    return [...options.values()]
+  }, [id, preFilteredRows])
+
+  return (
+    <select
+      value={filterValue}
+      onChange={e => {
+        setFilter(e.target.value || undefined)
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => {
+        if (option.length < 3) { // To prevent showing LW/RW, etc.
+          return(
+            <option key={i} value={option}>
+              {option}
+            </option>
+          );
+        }
+        return options
+      } 
+      )}
+      <option value={'/'}>Multi</option>
     </select>
   )
 }
