46587de849e3785173b2106a87c3d631f2925f8d
Paul Rupke <paul.rupke@gmail.com>
2020-12-18 22:56:31 -0500

    Add GoaliesTab

---
+++ src/components/AppWrapper/Tabs/GoaliesTab/GoaliesTab.jsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import { SearchColumnFilter, SelectPlayerTypeColumnFilter, SelectPositionColumnFilter } from '../../../Table/FilterTypes/FilterTypes';
+import Table from '../../../Table/Table';
+import Loading from '../../../Loading/Loading';
+
+
+export default function PlayersTab() {
+  const [players, setPlayers] = useState([]);
+
+  const columns = [
+    {
+      Header: 'Name',
+      accessor: 'name',
+      Filter: SearchColumnFilter,
+      sortType: 'alphanumeric',
+      width: '100px',
+    },
+    {
+      Header: 'Player Type',
+      accessor: 'prospect',
+      Filter: SelectPlayerTypeColumnFilter,
+      width: '0px',
+    },
+    {
+      Header: 'Team',
+      accessor: 'team',
+      Filter: SearchColumnFilter,
+      sortType: 'alphanumeric',
+      width: '100px',
+    },
+    {
+      Header: 'GS',
+      accessor: '18',
+      disableFilters: true,
+      sortType: 'alphanumeric',
+      width: '30px',
+    },
+    {
+      Header: 'W',
+      accessor: '19',
+      disableFilters: true,
+      sortType: 'alphanumeric',
+      width: '30px',
+    },
+    {
+      Header: 'GA',
+      accessor: '22',
+      disableFilters: true,
+      sortType: 'alphanumeric',
+      width: '30px',
+    },
+    {
+      Header: 'GAA',
+      accessor: '23',      
+      disableFilters: true,
+      sortType: 'alphanumeric',
+      width: '30px',
+    },
+    {
+      Header: 'SA',
+      accessor: '24',
+      disableFilters: true,
+      sortType: 'alphanumeric',
+      width: '30px',
+    },
+    {
+      Header: 'SV',
+      accessor: '25',
+      disableFilters: true,
+      sortType: 'alphanumeric',
+      width: '30px',
+    },
+    {
+      Header: 'SV%',
+      accessor: '26',
+      disableFilters: true,
+      sortType: 'alphanumeric',
+      width: '30px',
+    },
+    {
+      accessor: 'careerGP',
+    },
+    {
+      accessor: 'player_id',
+    },
+    {
+      accessor: 'player_key',
+    },
+  ]
+
+  const tableState = { 
+    hiddenColumns: ['player_id', 'player_key', 'careerGP'],
+    sortBy: [
+      {
+        id: '19',
+        desc: true
+      }
+    ]
+  }
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('/get_db_players?position=G')
+    .then(res => res.json())
+    .then(data => {
+      // console.log("data: " + JSON.stringify(data.players, null, 4));
+      setPlayers(data.players);
+    })
+    .then(setIsLoading(false));
+  }, [])
+
+  return (
+    <>
+      { isLoading &&
+        <Loading />
+      }
+      { !isLoading &&
+        <Table
+          data={players}
+          columns={columns}
+          tableState={tableState}
+          defaultColumn='name'
+          tableType="draft"
+        />
+      }
+    </>
+  );
+}
