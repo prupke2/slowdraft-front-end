0e6f631c3fcfb515463a29e1a78fb4693e51ca9f
Paul Rupke <paul.rupke@gmail.com>
2020-12-18 23:00:44 -0500

    Update AppWrapper

--- src/components/AppWrapper/AppWrapper.jsx
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -3,9 +3,8 @@ import Navbar from './Navbar/Navbar';
 import Chat from './Chat/Chat';
 
 export default function AppWrapper({logout, pub, sub, setLoadingText}) {
- 
   return (
-    <React.Fragment>
+    <>      
       <Navbar 
         logout={logout}
       />
@@ -14,6 +13,6 @@ export default function AppWrapper({logout, pub, sub, setLoadingText}) {
         sub={sub}
         setLoadingText={setLoadingText}
       />
-    </React.Fragment>
+    </>
   );
 }
