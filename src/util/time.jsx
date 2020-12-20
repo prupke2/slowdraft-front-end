8e162aad3820a6a384e163dbaa00badd95c47ccf
Paul Rupke <paul.rupke@gmail.com>
2020-12-19 20:45:11 -0500

    Add timeSince function

---
+++ src/util/time.jsx
@@ -0,0 +1,23 @@
+export function timeSince(ts) {
+  const timeStamp = new Date(ts);
+  const now = new Date(),
+    secondsPast = (now.getTime() - timeStamp) / 1000;
+  if (secondsPast < 60) {
+    return parseInt(secondsPast) + 's ago';
+  }
+  if (secondsPast < 3600) {
+    return parseInt(secondsPast / 60) + 'm ago';
+  }
+  if (secondsPast <= 86400) {
+    return parseInt(secondsPast / 3600) + 'h ago' ;
+  }
+  if (secondsPast <= 2629800) {
+    return parseInt(secondsPast / 86400) + 'days ago' ;
+  }
+  if (secondsPast > 2629800) {
+    const day = timeStamp.getDate();
+    const month = timeStamp.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ", "");
+    const year = timeStamp.getFullYear() === now.getFullYear() ? "" : timeStamp.getFullYear();
+    return month + " " + day + ", " + year;
+  }
+}
