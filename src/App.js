8abafbcaadc0ede5f0fb8fd84b4c68c752c3e344
Paul Rupke <paul.rupke@gmail.com>
2020-12-27 10:50:43 -0500

    use useRequest in app component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AppWrapper from './components/AppWrapper/AppWrapper';
 import Loading from './components/Loading/Loading';
 import './App.css';
 import { ToastsContainer, ToastsStore, ToastsContainerPosition } from 'react-toasts';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import useRequest from './util/useRequest';
 
 export default function App() {
 
@@ -12,91 +14,76 @@ export default function App() {
   // pub and sub (publish/subscribe) states are used for the chat backend
   const [pub, setPub] = useState(""); 
   const [sub, setSub] = useState("");
-  const [error, setError] = useState("");
-  const [loadingText, setLoadingText] = useState("Loading...");
   const [isLoading, setIsLoading] = useState(true);
   const queryParams = qs.parse(window.location.search);
   const code = queryParams["code"];
 
-  console.log("loggedIn: " + loggedIn);
+  const { data, loading, requestError } = useRequest('/check_login');
+  
+  function checkLogin() {
+    if (typeof(code) === "undefined") {
+      if (data.success === true) {
+        setPub(data.pub);
+        setSub(data.sub);
+        window.history.replaceState({}, document.title, "/");
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+      setIsLoading(false)
+    }
+  }
 
   useEffect(() => {
-    console.log("In App useEffect"); 
-    // This is called when the user goes to the site for the first time.
-    // If they already have a login session stored, they are logged in.
-    if (typeof(code) === "undefined") {
-      try {
-        console.log("fetching check_login");
-        fetch('/check_login')
-          .then(res => res.json())
-          .then(data => {
-            if (data.success === true) {
-              setPub(data.pub);
-              setSub(data.sub);
-              window.history.replaceState({}, document.title, "/");
-              setLoggedIn(true);
-            } else {
-              setLoggedIn(false);
-            }
-            console.log("setting isLoading to false");
-            setIsLoading(false)
-          }
-        );
-      } catch(error) {
-        console.log("Error in App useEffect")
-        setError("Unable to reach Yahoo. " + error)
-        setIsLoading(false);
-      }
-    } else {
-      setIsLoading(false);
+    if (!loggedIn) {
+      checkLogin();
+      
     }
-  }, [code]);
-  
+  }, [data]);
+
   function logout() {
     fetch('/logout').then(res => res.json()).then(data => {
       console.log("Logging out: " + data.success);
       setLoggedIn(false);
       setIsLoading(false);
-      setLoadingText("Loading...");
     });
   }
 
   return (
-    <div>
-      <ToastsContainer 
-        store={ToastsStore}
-        position={ToastsContainerPosition.TOP_CENTER}
-      />
-      { error && (
-        <div>{error}</div>
-      )}
-      { isLoading && (
-        <Loading 
-          text={loadingText}
+    <>
+      <ErrorBoundary>
+        <ToastsContainer 
+          store={ToastsStore}
+          position={ToastsContainerPosition.TOP_CENTER}
         />
-        )
-      }
-      { (!isLoading && !loggedIn) && (
-        <Login 
-          code={code}
-          setLoggedIn={setLoggedIn}
-          setPub={setPub}
-          setSub={setSub}
-          setIsLoading={setIsLoading}
-          setLoadingText={setLoadingText}
-        />
-      )}
-      { loggedIn && (
-        <main>
-          <AppWrapper
-            logout={logout}
-            pub={pub}
-            sub={sub}
-            setLoadingText={setLoadingText}
-          />
-        </main>
-      )}
-    </div>
+        { loading &&
+          <Loading />
+        }
+        { (!loading && !loggedIn) &&
+          <ErrorBoundary>          
+            <Login 
+              code={code}
+              setLoggedIn={setLoggedIn}
+              setPub={setPub}
+              setSub={setSub}
+              setIsLoading={setIsLoading}
+            />
+          </ErrorBoundary>
+        }
+        { loggedIn &&
+          <main>
+            <ErrorBoundary >
+              <AppWrapper
+                logout={logout}
+                pub={pub}
+                sub={sub}
+              />
+            </ErrorBoundary>
+          </main>
+        }
+      </ErrorBoundary>
+
+    </>
   );
 }
 
