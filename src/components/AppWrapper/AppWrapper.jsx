5de3793fa7a12b6cd80b72a07cd860e48e37b609
Paul Rupke <paul.rupke@gmail.com>
2020-12-28 21:37:50 -0500

    get team session in app wrapper

--- src/components/AppWrapper/AppWrapper.jsx
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './Navbar/Navbar';
 import Chat from './Chat/Chat';
+import dummyIcon from '../../assets/dummy_icon.png';
+import Logo from './Navbar/Logo';
 
 export default function AppWrapper({logout, pub, sub, setLoadingText}) {
+  const [yahooTeamId, setYahooTeamId] = useState(null);
+  const [teamLogo, setTeamLogo] = useState(dummyIcon);
+  const [teamName, setTeamName] = useState('');
+
+  function getYahooTeam() {
+    fetch('/get_team_session')
+      .then(async response => {
+        const data = await response.json();
+        if (!response.ok) {
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+        }
+        setYahooTeamId(data.user_id);
+        setTeamLogo(data.logo);
+        setTeamName(data.team_name);
+      });
+  }
+  
+  useEffect(() => {
+    if (!yahooTeamId) {
+      console.log("Getting yahooTeamId")
+      getYahooTeam();
+    }
+  }, []);
+
   return (
     <>      
       <Navbar 
         logout={logout}
       />
+      <Logo 
+        teamLogo={teamLogo}
+        teamName={teamName}
+      />
       <Chat 
         pub={pub}
         sub={sub}
-        setLoadingText={setLoadingText}
+        teamName={teamName}
       />
     </>
   );
