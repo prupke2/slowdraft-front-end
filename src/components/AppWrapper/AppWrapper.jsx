bcfd7699e87b7c27b2f02dcb878e67a6dafa8e9e
Paul Rupke <paul.rupke@gmail.com>
2020-12-30 12:52:55 -0500

    Add chat back

--- src/components/AppWrapper/AppWrapper.jsx
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -52,11 +52,11 @@ export default function AppWrapper({logout, pub, sub}) {
         userPickingNow={userPickingNow}
         pickExpiry={pickExpiry}
       />
-      {/* <Chat 
+      <Chat 
         pub={pub}
         sub={sub}
         teamName={teamName}
-      /> */}
+      />
     </>
   );
 }
