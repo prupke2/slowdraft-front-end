04b1ff8a55837df95892677858939ff85944cfb2
Paul Rupke <paul.rupke@gmail.com>
2020-12-31 00:44:58 -0500

    scroll to bottom when chat message received

--- src/components/AppWrapper/Chat/MessageLog/MessageLog.jsx
+++ src/components/AppWrapper/Chat/MessageLog/MessageLog.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { useEffect } from 'react';
 
-export default function MessageLog(props) {
+export default function MessageLog({messages}) {
+  const chatbox = document.querySelector("ul#chat-messages");
+
+  // Scroll to bottom of chatbox on load and when a new message comes in
+  useEffect(() => {
+    console.log("typeof(chatbox): " + typeof(chatbox));
+    if (chatbox) { 
+      chatbox.scrollTop = chatbox.scrollHeight;
+    }
+  }, [messages]);
+
   return(
     <ul id="chat-messages">
       <li>
         <div>
-          { props.messages.map((item, index)=>(
+          { messages.map((item, index)=>(
             <div key={index}>
-              <span className="user">{item.uuid}: </span>
-              <span className="message">{item.text}</span>
+              { item.uuid !== '***' &&
+                <>
+                  <span className="user">{item.uuid} </span>
+                  <span className="message">{item.text}</span>
+                </>
+              }
+              { item.uuid === '***' &&
+                <span className="message chat-announcement">*** {item.text}</span>
+              }
             </div>
           )) }
         </div>
