3a78120baa24a14b686ca7d5259d43e216202d53
Paul Rupke <paul.rupke@gmail.com>
2021-01-02 15:05:24 -0500

    check for updates every 30 seconds

--- src/components/AppWrapper/AppWrapper.jsx
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -17,6 +17,7 @@ export default function AppWrapper({logout, pub, sub}) {
   const channel = "test" // To reset messages, update the channel name to something new
   const [messages, setMessages] = useState([]);
 
+  console.log("currentPick: " + JSON.stringify(currentPick, null, 4))
   function getYahooTeam() {
     fetch('/get_team_session')
       .then(async response => {
@@ -51,10 +52,61 @@ export default function AppWrapper({logout, pub, sub}) {
   
   useEffect(() => {
     if (localStorage.getItem( 'yahooSession' ) !== true) {
-      if (!userId) {
+      if (!userId || teamName === '') {
         console.log("Getting userId")
         getYahooTeam();
       }
+    }
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() =>
+      fetch('/check_for_updates')
+      .then(async response => {
+        const now = new Date()
+        const data = await response.json();
+        if (!response.ok) {
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+        }
+        if (Date.parse(data.updates.latest_draft_update) > Date.parse(localStorage.getItem('draftDataUpdate'))) {
+          console.log("Update draft data...");
+          fetch('/get_draft')
+          .then(res => res.json())
+          .then(data => {
+            // console.log("data: " + JSON.stringify(data, null, 4))
+            localStorage.setItem('draftData', JSON.stringify(data))
+            localStorage.setItem('draftDataUpdate', new Date())
+    
+            setPicks(data.picks);
+            if (typeof(data.current_pick) !== 'undefined') {
+              // setUserPickingNow(data.current_pick);
+              // setPickExpiry(data.current_pick.pick_expires);
+              setCurrentPick(data.current_pick);
+            }
+          })
+        } 
+        
+        // if (Date.parse(data.updates.latest_team_update) > Date.parse(localStorage.getItem('teamsDataUpdate'))) {
+        //   console.log("Update draft data...");
+        //   fetch('/get_draft')
+        //   .then(res => res.json())
+        //   .then(data => {
+        //     // console.log("data: " + JSON.stringify(data, null, 4))
+        //     localStorage.setItem('teamsData', JSON.stringify(data))
+        //     localStorage.setItem('teamsDataUpdate', new Date())
+        //     }
+        //   })
+        // }
+
+        console.log("data.latest_draft_update: " + data.updates.latest_draft_update);
+        console.log("data update: " + data.updates.latest_draft_update + '  ' + Date.parse(data.updates.latest_draft_update));
+        console.log("localStorage: " + localStorage.getItem('draftDataUpdate') + Date.parse(localStorage.getItem('draftDataUpdate')));
+      })
+      , 30000
+    );
+    return () => {
+      clearInterval(interval);
     }
   }, []);
 
