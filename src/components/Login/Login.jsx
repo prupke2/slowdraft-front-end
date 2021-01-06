ce53fe183690ae5e9c99f845eb1ce65b34e49b3d
Paul Rupke <paul.rupke@gmail.com>
2021-01-05 22:47:07 -0500

    remove unused code in login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,11 +2,9 @@ import React, { useEffect } from 'react';
 import Errors from '../Errors/Errors';
 import './Login.css';
 import { ToastsStore } from 'react-toasts';
-// import { getCookie } from '../../assets/Cookies';
 
-export default function Login({ code, setLoggedIn, setPub, setSub, setIsLoading, 
-    // setUserId, setTeamLogo, setTeamName, setRole, setColor, setLeagueId 
-    user, setUser
+export default function Login({ code, setLoggedIn, setPub, setSub, 
+  setIsLoading, user, setUser
   }) {
   const client_id = "dj0yJmk9ZXVsUnFtMm9hSlRqJmQ9WVdrOU1rOU5jWGQzTkhNbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWQ1";
   let errors = null;
@@ -28,33 +26,20 @@ export default function Login({ code, setLoggedIn, setPub, setSub, setIsLoading,
             setSub(data.sub);
             localStorage.setItem( 'pub', data.pub );
             localStorage.setItem( 'sub', data.sub );
-            // let user = {
-            //   'user_id': data.user_id, 
-            //   'logo': data.logo,
-            //   'team_name': data.team_name, 
-            //   'league_id': data.team_id, 
-            //   'role': data.role, 
-            //   'color': data.color
-            // }
             setUser(JSON.stringify(data.user));
             localStorage.setItem( 'user', JSON.stringify(data.user) );
-            // console.log("setting user to: " + JSON.parse(data.user, null, 4))
-            // setUserId(data.user.user_id);	
-            // setTeamLogo(data.user.logo);	
-            // setTeamName(data.user.team_name);	
-            // setRole(data.user.role);	
-            // setColor(data.user.color);
-            // setLeagueId(data.user.league_id);
             setLoggedIn(true);
             setIsLoading(false);
+            const draftTab = document.getElementById('react-tabs-0');
+            draftTab.click();
           } else {
-            // setLoggedIn(false);
             setIsLoading(false)
           }
         })
         .catch( err => {
           ToastsStore.error(`There was an error connecting to the server. Please try again later.`);
           console.log(`Error: ${err.text}`);
+          setLoggedIn(false);
         })
     }
     if (typeof(code) !== 'undefined') {
