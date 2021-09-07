a629ad57bcd478ce1cb5fb0db4ca1e73447f919c
Paul Rupke <paul.rupke@gmail.com>
2021-09-06 20:45:13 -0400

    update sheet

--- src/components/AppWrapper/Tabs/PickTrackerTab/PickTrackerTab.jsx
+++ src/components/AppWrapper/Tabs/PickTrackerTab/PickTrackerTab.jsx
@@ -5,7 +5,7 @@ export default function PickTrackerTab() {
   return (
     <iframe
       className='picks-iframe'
-      src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQjGUyaVWR-sDOCvWx2Ue6OpRiK2OiN9bgmTRN6Nv4902raikLi06ItB-E6jX4-ImBIGpAk7lZPZfsL/pubhtml?gid=1591754969&amp;single=true&amp;widget=true&amp;headers=false' 
+      src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQjGUyaVWR-sDOCvWx2Ue6OpRiK2OiN9bgmTRN6Nv4902raikLi06ItB-E6jX4-ImBIGpAk7lZPZfsL/pubhtml?widget=true'
     />
   )
 }
