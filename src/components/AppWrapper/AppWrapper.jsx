ce16530050de255e30c595ce769a857b9f813734
Paul Rupke <paul.rupke@gmail.com>
2021-09-07 23:41:56 -0400

    add optional user param to getLatestData

--- src/components/AppWrapper/AppWrapper.jsx
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -18,8 +18,9 @@ export default function AppWrapper({setLoggedIn, logout, pub, sub, user, setUser
   const [posts, setPosts] = useState([]);
   const [rules, setRules] = useState([]);
 
-  function getLatestData(draftOnly = false) {
-    checkForUpdates(draftOnly, user, setPicks, setCurrentPick, setDraftingNow, setPlayers, setGoalies, 
+  function getLatestData(draftOnly = false, optionalUserParam = null) {
+    const userToCheck = optionalUserParam === null ? user : optionalUserParam;
+    checkForUpdates(draftOnly, userToCheck, setPicks, setCurrentPick, setDraftingNow, setPlayers, setGoalies, 
       setTeams, setRules, setPosts, getDraft, getDBPlayers, getDBGoalies, getTeams, getRules, getForumPosts)
   }
 
@@ -99,6 +100,7 @@ export default function AppWrapper({setLoggedIn, logout, pub, sub, user, setUser
         sub={sub}
         user={user}
         channel={channel}
+        getLatestData={getLatestData}
       />
     </>
   );
