737ecc40166094b4e4286d521cf703ccaaed4a20
Paul Rupke <paul.rupke@gmail.com>
2021-09-10 21:56:54 -0400

    Update user icon/name styling

--- src/components/AppWrapper/UsernameStyled/UsernameStyled.jsx
+++ src/components/AppWrapper/UsernameStyled/UsernameStyled.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './UsernameStyled.css';
 
 export default function UsernameStyled({username, color}) {
   return (
-    <p>
-      <span role='img' aria-label='icon' style={{ 'background': color}}>👤</span>
-      <span className="user">{username}</span>
+    <p className='user-wrapper'>
+      <span role='img' aria-label='icon' className="user-icon" style={{ 'background': color}}>👤</span>
+      <span className="userInDraft">{username}</span>
     </p>
   );
 }
