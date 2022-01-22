cbbc3eed3fd484f401211b656ab02cc7a5a8a445
Paul Rupke <paul.rupke@gmail.com>
2022-01-22 15:01:33 -0500

    add localEnvironment util function

--- src/util/util.jsx
+++ src/util/util.jsx
@@ -1,3 +1,8 @@
 export function substringInString(string, substring) {
   return string.indexOf(substring) !== -1
 }
+
+export function localEnvironment() {
+  const url = window.location.href;
+  return substringInString(url, "localhost:3000") || substringInString(url, "0.0.0.0:3000/");
+}
