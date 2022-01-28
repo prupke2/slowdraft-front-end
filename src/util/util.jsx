f538276790a22640c7af98380bf27526fc418363
Paul Rupke <paul.rupke@gmail.com>
2022-01-27 21:03:06 -0500

    add util functions

--- src/util/util.jsx
+++ src/util/util.jsx
@@ -1,3 +1,16 @@
+
+export const user = JSON.parse(localStorage.getItem('user'));
+export const teams = JSON.parse(localStorage.getItem('teams'));
+export const webToken = localStorage.getItem('web_token');
+
+export const getHeaders = () => (
+  {
+    'Accept': 'application/json', 
+    'Content-Type': 'application/json',
+    'Authorization': localStorage.getItem('web_token')
+  }
+);
+
 export function substringInString(string, substring) {
   return string.indexOf(substring) !== -1
 }
@@ -5,4 +18,8 @@ export function substringInString(string, substring) {
 export function localEnvironment() {
   const url = window.location.href;
   return substringInString(url, "localhost:3000") || substringInString(url, "0.0.0.0:3000/");
+}
+
+export function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
