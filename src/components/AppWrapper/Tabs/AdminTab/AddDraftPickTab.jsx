0460ae0caa21f633191e65d5cba6d17526c96d64
Paul Rupke <paul.rupke@gmail.com>
2022-01-30 20:36:06 -0500

    update request headers in AddDraftPickTab

--- src/components/AppWrapper/Tabs/AdminTab/AddDraftPickTab.jsx
+++ src/components/AppWrapper/Tabs/AdminTab/AddDraftPickTab.jsx
@@ -1,30 +1,27 @@
 import React, { useState } from 'react';
 import { ToastsStore } from 'react-toasts';
-import { localEnvironment } from '../../../../util/util';
-// import SelectPlayerDropdown from './SelectPlayerDropdown';
+import { getHeaders, teamIdToKey, teamsMap } from '../../../../util/util';
+import Emoji from '../../Emoji';
 
-export default function AddDraftPickTab({ userInfo }) {
-  const [userId, setUserId] = useState(351);
-
+export default function AddDraftPickTab() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const teams = JSON.parse(localStorage.getItem('teams'));
+  const [teamId, setTeamId] = useState(user.team_id);
   function addNewPick(e) {
     e.preventDefault();
     const requestParams = {
       method: 'POST',
-      headers: { 
-        'Accept': 'application/json', 
-        'Content-Type': 'application/json'
-      },
+      headers: getHeaders(),
       body: JSON.stringify({ 
-        user_id: userId,
-        league_id: userInfo.league_id,
-        draft_id: userInfo.draft_id
+        team_key: teamIdToKey(teamId)
       })
     }
-    if ( userId ) {    
+    if ( teamId ) {    
       fetch('/add_new_pick', requestParams)
         .then(response => {
         if (!response.ok) {
           const error = response.status;
+          console.log(`error: ${JSON.stringify(error, null, 4)}`);
           return Promise.reject(error);
         }
         return response.json()
@@ -33,7 +30,7 @@ export default function AddDraftPickTab({ userInfo }) {
         if (data.success === true) {
           setTimeout(function () {
             ToastsStore.success('Pick added successfully.')
-          }, 1000)
+          }, 200)
         } else {
           ToastsStore.error('Error adding pick.')
         }
@@ -43,37 +40,26 @@ export default function AddDraftPickTab({ userInfo }) {
     }
   }
 
-  const handleUserIdChange = event => {
-    setUserId(event.target.value)
+  const handleTeamIdChange = event => {
+    setTeamId(event.target.value)
   };
   
   return (
     <form className='admin-form add-keeper-form'>
       <h2>Add draft pick</h2>
-      <p className='instructions'>
-        <span role='img' aria-label='instructions'>⚠️</span> This will add a pick at the end of the draft for the specified team.
-      </p>
+      <div className='instructions'>
+        <div>
+          <Emoji emoji='⚠️' />
+          This will add a pick at the end of the draft for the specified team.
+        </div>
+      </div>
       <div>
         <label name='admin-user-dropdown'>Team:</label>
         <select 
           className='admin-user-dropdown'
-          onChange={handleUserIdChange}
+          onChange={handleTeamIdChange}
         >
-          <option value={351}>American Gladiators</option>
-          <option value={441}>Fort Wayne Komets</option>
-          <option value={301}>GrandRapids Griffins</option>
-          <option value={371}>Nelson Leafs</option>
-          <option value={381}>New Orleans Brass</option>
-          <option value={321}>Ontario Reign</option>
-          <option value={341}>Providence Bruins</option>
-          <option value={331}>Seaforth Generals</option>
-          <option value={411}>St. Marys Lincolns</option>
-          <option value={391}>St. Thomas Stars</option>
-          <option value={431}>Syracuse Crunch!</option>
-          <option value={361}>Terrace River Kings</option>
-          { localEnvironment &&
-            <option value={292}>LOCAL TESTING ONLY</option>
-          }
+          { teamsMap(teams) }
         </select>
       </div>
       <br />
