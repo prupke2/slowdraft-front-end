babb916ccb6832e6837f662ff110dd9e41449ff3
Paul Rupke <paul.rupke@gmail.com>
2022-02-01 23:24:15 -0500

    update UsernameStyled component

--- src/components/AppWrapper/UsernameStyled/UsernameStyled.jsx
+++ src/components/AppWrapper/UsernameStyled/UsernameStyled.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './UsernameStyled.css';
 
-export default function UsernameStyled({username, color}) {
+export default function UsernameStyled({username, color, teamId}) {
+  const teams = JSON.parse(localStorage.getItem('teams'));
   return (
     <p className='user-wrapper'>
-      <span role='img' aria-label='icon' className="user-icon" style={{ 'background': color}}>👤</span>
+      { (teamId && teams) &&
+        <img className='user-logo' src={teams[teamId - 1].team_logo || null} alt='👤' />
+      }
+      { !teamId &&
+        <span role='img' aria-label='icon' className="user-icon" style={{ 'background': color}}>👤</span>
+      }
       <span className="userInDraft">{username}</span>
     </p>
   );
