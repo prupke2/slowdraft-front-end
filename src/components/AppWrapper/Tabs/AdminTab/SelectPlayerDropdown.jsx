c8dbe3def2dc414331f14d200bce885867e70f00
Paul Rupke <paul.rupke@gmail.com>
2022-02-03 20:41:24 -0500

    update SelectPlayerDropdown

--- src/components/AppWrapper/Tabs/AdminTab/SelectPlayerDropdown.jsx
+++ src/components/AppWrapper/Tabs/AdminTab/SelectPlayerDropdown.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import { substringInString } from '../../../../util/util';
 
-export default function SelectPlayerDropdown({ onClick, setPlayerId, buttonName }) {
+export default function SelectPlayerDropdown({ handleClick, setPlayerId, buttonName, formComplete }) {
   const playerData = JSON.parse(localStorage.getItem('playerDBData'));
   const goalieData = JSON.parse(localStorage.getItem('goalieDBData'));
-  const [text, setText] = useState("");
-
-
+  const [text, setText] = useState('');
   function handleKeeperText(e) {
     e.preventDefault();
     setText(e.target.value);
@@ -15,45 +13,61 @@ export default function SelectPlayerDropdown({ onClick, setPlayerId, buttonName 
   const playerDiv = (player) => {
     function handlePlayerSelect() {
       setPlayerId(player.player_id);
-      setText(player.name);
+      setText(`${player.name}`);
     }
-    if (text && substringInString(player.name.toUpperCase(), text.toUpperCase())) {
-      return (
-        <div 
-          key={player.player_id}
-          value={player.player_id}
-          onClick={handlePlayerSelect}
-        >
-          {player.name}, {player.position}, {player.team}
-        </div>
-      )
+    const match = text !== '' && substringInString(player.name.toUpperCase(), text.toUpperCase());
+    if (!match) {
+      return null;
     }
-    return null;
+    const prospect = player.prospect === '1' ? '(prospect)' : '';
+    return (
+      <>
+        {match &&
+          <div 
+            key={player.player_id}
+            value={player.player_id}
+            onClick={handlePlayerSelect}
+          >
+            {player.name}, {player.position}, {player.team} {` `}
+            {prospect &&
+              <span>&nbsp;
+                <span className='prospect' title='Prospect'>P</span>
+              </span>
+            }
+          </div>
+        }
+      </>
+    )
   } 
 
   return (
     <>
       <div>
-        <label name='player'>Player:</label>
-        <div className="player-search">
+        <label className='player-label' name='player'>Player:</label>
+        <div className='player-search'>
           <input
-            className="player-search-input"
-            type="text"
+            className='player-search-input'
+            type='text'
             value={text}
-            placeholder="Find player..."
+            placeholder='Find player...'
             onChange={handleKeeperText}
           />
-          <button id="clear-field" onClick={e => {
+          <button id='clear-field' onClick={e => {
             e.preventDefault();
             setText('');
           }}>x</button>
-          <div className="player-list-wrapper">
+          <div className='player-list-wrapper'>
             {playerData.players.map((player, i) => playerDiv(player, i))}
             {goalieData.players.map((player, i) => playerDiv(player, i))}
           </div>
         </div>
       </div>
-      <button onClick={(e) => onClick(e)} className="add-player-button">{ buttonName }</button>
+      <button 
+        onClick={(e) => handleClick(e)} 
+        className='add-player-button'
+        disabled={!formComplete}
+        >{ buttonName }
+      </button>
     </>
   )
 }
