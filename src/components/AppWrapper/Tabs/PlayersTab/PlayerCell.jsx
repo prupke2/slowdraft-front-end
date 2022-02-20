1fa3b546151e6be9cb227941f35fd67b4ced7b12
Paul Rupke <paul.rupke@gmail.com>
2022-02-19 23:00:12 -0500

    add PlayerCell component

---
+++ src/components/AppWrapper/Tabs/PlayersTab/PlayerCell.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const PlayerCell = ({cell, takenPlayer, draftingNow}) => {
+  return (
+    <div className="player-name">
+      {cell.row.original.player_id && 
+        <div className='player-name-and-headshot'>
+          <img className='headshot' src={cell.row.original.headshot} alt='' />
+          <span>
+            <a
+              href={`https://sports.yahoo.com/nhl/players/${cell.row.original.player_id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {cell.row.original.prospect === "1" && 
+                <span>
+                  <span className='prospect' title='Prospect'>P</span>
+                  &nbsp;
+                </span>
+              }
+              {cell.row.original.is_keeper === 1 && 
+                <span>
+                  <span className='keeper' title='Keeper'>K</span>
+                  &nbsp;
+                </span>
+              }
+              {cell.value}
+            </a>
+            { (takenPlayer && !draftingNow) &&
+              <div className="small-username">
+                &nbsp;{cell.row.original.user}
+              </div>
+            }
+          </span>
+        </div>
+      }
+      { !cell.row.original.player_id && 
+        <span>{cell.value}</span>
+      }
+    </div>
+  );
+}
+
+export default PlayerCell;
