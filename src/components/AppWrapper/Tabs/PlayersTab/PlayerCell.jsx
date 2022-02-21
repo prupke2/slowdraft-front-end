2ac2fffab486c846d205acc1f3dba6e660ed17cb
Paul Rupke <paul.rupke@gmail.com>
2022-02-20 20:52:31 -0500

    adjust playerCell

--- src/components/AppWrapper/Tabs/PlayersTab/PlayerCell.jsx
+++ src/components/AppWrapper/Tabs/PlayersTab/PlayerCell.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-const PlayerCell = ({cell, takenPlayer, draftingNow}) => {
+const PlayerCell = ({cell, draftingNow}) => {
+  const takenPlayer = cell.row.original.user !== null ? 'taken-player' : null;
   return (
-    <div className="player-name">
+    <div className='player-name'>
       {cell.row.original.player_id && 
         <div className='player-name-and-headshot'>
           <img className='headshot' src={cell.row.original.headshot} alt='' />
           <span>
             <a
               href={`https://sports.yahoo.com/nhl/players/${cell.row.original.player_id}`}
-              target="_blank"
-              rel="noopener noreferrer"
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              {cell.row.original.prospect === "1" && 
+              {cell.row.original.prospect === '1' && 
                 <span>
                   <span className='prospect' title='Prospect'>P</span>
                   &nbsp;
@@ -27,7 +28,7 @@ const PlayerCell = ({cell, takenPlayer, draftingNow}) => {
               {cell.value}
             </a>
             { (takenPlayer && !draftingNow) &&
-              <div className="small-username">
+              <div className='small-username'>
                 &nbsp;{cell.row.original.user}
               </div>
             }
