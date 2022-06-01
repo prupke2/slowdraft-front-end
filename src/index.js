3a04fb2daa0aeea17b83d63d01c06d56f44c129e
Paul Rupke <paul.rupke@gmail.com>
2022-05-31 20:19:40 -0400

    add store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { HashRouter } from "react-router-dom";
+import store from './store';
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
