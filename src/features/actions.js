7b8394cfb24826b3d60ebee4f3d79b4e80b9e7ce
Paul Rupke <paul.rupke@gmail.com>
2022-05-31 20:18:20 -0400

    add actions

---
+++ src/features/actions.js
@@ -0,0 +1,6 @@
+
+export const SET_DRAFT = 'set_draft';
+export const SET_TEAMS = 'set_teams';
+export const SET_POSTS = 'set_posts';
+export const SET_RULES = 'set_rules';
+export const SET_TRADES = 'set_trades';
