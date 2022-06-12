db024280186cb9a31972b927b5bcd20e4fe0d830
Paul Rupke <paul.rupke@gmail.com>
2022-06-11 21:52:42 -0400

    fix user to team page links

--- src/components/AppWrapper/UsernameStyled/UsernameStyled.jsx
+++ src/components/AppWrapper/UsernameStyled/UsernameStyled.jsx
@@ -1,37 +1,36 @@
 import React from "react";
 import "./UsernameStyled.css";
 import { Link } from "react-router-dom";
-import Emoji from "../Emoji";
 
 export default function UsernameStyled({ username, color, teamId }) {
   const teams = JSON.parse(localStorage.getItem("teams"));
+
   return (
     <div className="user-wrapper">
-      {teamId && teams && (
-        <img
-          className="user-logo"
-          src={teams[teamId - 1].team_logo || null}
-          alt="👤"
-        />
-      )}
-      {!teamId && (
-        <span
-          role="img"
-          aria-label="icon"
-          className="user-icon"
-          style={{ background: color }}
-        >
-          <Link
-            to={{
-              pathname: `/teams`,
-              state: { teamId: `${teamId}` },
-            }}
+      <Link
+        to={{
+          pathname: `/teams`,
+          state: { teamId: `${teamId}` },
+        }}
+      >
+        {teamId && teams && (
+          <img
+            className="user-logo"
+            src={teams[teamId - 1].team_logo || null}
+            alt="👤"
+          />
+        )}
+        {!teamId && (
+          <span
+            role="img"
+            aria-label="icon"
+            className="user-icon"
+            style={{ background: color }}
           >
-            <Emoji emoji="👤" />
-          </Link>
-        </span>
-      )}
-      <span className="user-in-draft">{username}</span>
+          </span>
+        )}
+        <span className="user-in-draft">{username}</span>
+      </Link>
     </div>
   );
 }
