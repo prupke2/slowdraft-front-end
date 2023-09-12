252b4ebce545bf70fdef22819604571cd8f5f1e4
Paul Rupke <paul.rupke@gmail.com>
2023-09-11 21:18:35 -0400

    update pick tracker tab

--- src/components/AppWrapper/Tabs/PickTrackerTab/PickTrackerTab.jsx
+++ src/components/AppWrapper/Tabs/PickTrackerTab/PickTrackerTab.jsx
@@ -1,24 +1,51 @@
 import React, { useEffect, useState } from "react";
 import "./PickTrackerTab.css";
 import Loading from "../../../Loading/Loading";
+import { API_URL, getHeaders } from "../../../../util/util";
 
 export default function PickTrackerTab() {
   const [isLoading, setIsLoading] = useState(true);
+  const [docURL, setDocURL] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    setTimeout(function () {
+    if (!docURL) {
+      try {
+        fetch(`${API_URL}/get_doc_url`, {
+          method: "GET",
+          headers: getHeaders(),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.success === false) {
+              setError(data.error);
+            } else {
+              setDocURL(data.url);
+            }
+          });
+        }
+      catch (err) {
+        setError(err);
+        console.log('Error fetching doc URL:', err);
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
       setIsLoading(false);
-    }, 1000);
-  }, []);
+    }
+  }, [docURL]);
 
   return (
-    <>
+    <div className="pick-tracker-wrapper">
       {isLoading && <Loading absolute={true} />}
-      <iframe
-        title="Pick Tracker spreadsheet"
-        id="picks-iframe"
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQjGUyaVWR-sDOCvWx2Ue6OpRiK2OiN9bgmTRN6Nv4902raikLi06ItB-E6jX4-ImBIGpAk7lZPZfsL/pubhtml?widget=true"
-      />
-    </>
+      {error && <div className="error">Error loading spreadsheet: {error}</div>}
+      {docURL && !isLoading && (
+        <iframe
+          title="Pick Tracker spreadsheet"
+          id="picks-iframe"
+          src={docURL}
+        />
+      )}
+    </div>
   );
 }
