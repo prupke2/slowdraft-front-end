8f7a2573092afaa766c5f3219f57392b41e05171
Paul Rupke <paul.rupke@gmail.com>
2023-09-23 22:50:21 -0400

    update Emoji

--- src/components/AppWrapper/Emoji.jsx
+++ src/components/AppWrapper/Emoji.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./AppWrapper.css";
 
-export default function Emoji({ emoji, navbar }) {
+export default function Emoji({ emoji, navbar, ariaLabel=null }) {
   const navbarClass = navbar ? "navbar-emoji" : null;
+  const emptyStarClass = emoji === '★' && 'empty-star';
   return (
-    <span className={navbarClass} role="img" aria-label="draft">
+    <span className={`${navbarClass} ${emptyStarClass}`} role="img" aria-label={ariaLabel}>
       {emoji}
     </span>
   );
