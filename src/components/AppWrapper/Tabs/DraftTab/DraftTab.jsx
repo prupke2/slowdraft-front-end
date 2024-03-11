c3f58a86fb2660496b39fce5a6b60517a3450bcd
Paul Rupke <paul.rupke@gmail.com>
2024-03-10 21:21:05 -0400

    update draft tab and modal

--- src/components/AppWrapper/Tabs/DraftTab/DraftTab.jsx
+++ src/components/AppWrapper/Tabs/DraftTab/DraftTab.jsx
@@ -3,7 +3,6 @@ import Table from "../../../Table/Table";
 import Loading from "../../../Loading/Loading";
 import {
   getDraft,
-  getDBGoalies,
   getDBPlayers,
   getTeams,
   offsetMilliseconds,
@@ -293,12 +292,8 @@ export default function DraftTab({
     }
     setIsLoading(false);
 
-    const localGoalieData = localStorage.getItem("goalieDBData");
     const localPlayerData = localStorage.getItem("playerDBData");
     const playerTeamData = localStorage.getItem("playerTeamData");
-    if (!localGoalieData) {
-      getDBGoalies();
-    }
     if (!localPlayerData) {
       getDBPlayers();
     }
