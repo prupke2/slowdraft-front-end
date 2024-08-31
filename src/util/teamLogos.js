52403a171149b698bae5aa298779e88c86f71ecc
Paul Rupke <paul.rupke@gmail.com>
2024-08-31 01:16:11 -0400

    import assets into file to export as object

---
+++ src/util/teamLogos.js
@@ -0,0 +1,40 @@
+import ANA from "../assets/ANA.png";
+import ARI from "../assets/ARI.png";
+import BOS from "../assets/BOS.png";
+import BUF from "../assets/BUF.png";
+import CAR from "../assets/CAR.png";
+import CBJ from "../assets/CBJ.png";
+import CGY from "../assets/CGY.png";
+import CHI from "../assets/CHI.png";
+import COL from "../assets/COL.png";
+import DAL from "../assets/DAL.png";
+import DET from "../assets/DET.png";
+import EDM from "../assets/EDM.png";
+import FLA from "../assets/FLA.png";
+import LA from "../assets/LA.png";
+import MIN from "../assets/MIN.png";
+import MTL from "../assets/MTL.png";
+import NJ from "../assets/NJ.png";
+import NSH from "../assets/NSH.png";
+import NYI from "../assets/NYI.png";
+import NYR from "../assets/NYR.png";
+import OTT from "../assets/OTT.png";
+import PHI from "../assets/PHI.png";
+import PIT from "../assets/PIT.png";
+import SEA from "../assets/SEA.png";
+import SJ from "../assets/SJ.png";
+import STL from "../assets/STL.png";
+import TB from "../assets/TB.png";
+import TOR from "../assets/TOR.png";
+import UTA from "../assets/UTA.png";
+import VAN from "../assets/VAN.png";
+import VGK from "../assets/VGK.png";
+import WPG from "../assets/WPG.png";
+import WSH from "../assets/WSH.png";
+
+const teamLogos = {
+  ANA, ARI, BOS, BUF, CAR, CBJ, CGY, CHI, COL, DAL, DET, EDM, FLA, LA, MIN, MTL,
+  NJ, NSH, NYI, NYR, OTT, PHI, PIT, SEA, SJ, STL, TB, TOR, UTA, VAN, VGK, WPG, WSH,
+}
+
+export default teamLogos;
